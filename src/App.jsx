256ec66ff567dd4c1e016a3cb272a9b3f661f757
prasad <prasadneroolkar90@gmail.com>
2024-07-22 21:16:56 +0530

    app jsx file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import Form from "./components/Form";
 import Table from "./components/Table";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const addValue = (tittle, cat, amt) => {
-    console.log(`added : ${tittle}, ${cat}, ${amt}`);
-  };
+  const [items, SetItems] = useState([]);
 
+  const addValue = (title, cat, amt) => {
+    console.log(`added : ${title}, ${cat}, ${amt}`);
+    const newArray = [...items, title, cat, amt];
+    SetItems(newArray);
+    console.log(newArray);
+  };
   return (
     <>
       <main>
         <h1>Track Your Expense</h1>
         <div className="expense-tracker">
           <Form onHandleAdd={addValue} />
-          <Table />
+          <Table itemsAdded={items} />
           <div className="context-menu">
             <div>Edit</div>
             <div>Delete</div>
